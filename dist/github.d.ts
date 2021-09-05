c36bd16a7e288622fc0ba9dd589f2c27a35bf966
Shunichi Ikegami <sike.tm@gmail.com>
2021-09-05 14:14:12 +0900

    reduce worst case github api calls

--- dist/github.d.ts
+++ dist/github.d.ts
@@ -5,4 +5,17 @@ export declare type Repository = {
 export declare type Option = {
     token?: string;
 };
+export declare type GithubFileEntry = {
+    path: string;
+    mode: string;
+    type: 'blob' | 'tree';
+    size: number;
+    sha: string;
+    url: string;
+};
 export declare function getContentUrl(repo: Repository, path: string, option?: Option): Promise<string>;
+export declare function getDefaultBranch(repo: Repository, option?: Option): Promise<string>;
+export declare function getDirectoryEntries(repo: Repository, params: {
+    branch: string;
+    path: string | null;
+}, option?: Option): Promise<GithubFileEntry[]>;
